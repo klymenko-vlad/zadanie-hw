e59cd0b3f56dda2e2127f72910fa57d8c0fa2322
Vlad Klymenko <klymenvlad@gmail.com>
2025-03-07 12:40:12 +0100

    feat: questions were moved to a separate file

---
+++ otazky.php
@@ -0,0 +1,15 @@
+<?php
+$otazky = array(
+    "Aké sú vaše skúsenosti s PHP?",
+    "Aký je váš obľúbený programovací jazyk?",
+    "Aké sú vaše ciele pre túto stránku?",
+    "Aké je najlepšie zviera?",
+);
+
+$odpovede = array(
+    "Mám základné znalosti PHP a skúsenosti, ktoré neviem použiť.",
+    "Môj obľúbený programovací jazyk bude určite PHP.",
+    "Mojím cieľom je prejsť tento predmet.",
+    "Odpoveď bude vždy kačica.",
+);
+?>
